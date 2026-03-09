ef305b0f10f24e9f57631ea67c55b2177c1de29f
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2026-03-09 17:42:00 -0400

    add links

--- src/funkin/utilities/CoolUtil.hpp
+++ src/funkin/utilities/CoolUtil.hpp
@@ -7,6 +7,9 @@ namespace funkin::utilities
     class CoolUtil
     {
         public:
+            /**
+             * @see https://gist.github.com/dgoguerra/7194777
+             */
             static std::string formatBytes(std::size_t bytes, std::uint8_t precision = 2);
     };
     
--- src/funkin/utilities/ProcessInfo.hpp
+++ src/funkin/utilities/ProcessInfo.hpp
@@ -3,12 +3,19 @@
 #include <cstddef>
 
 
-namespace funkin::utilities{
-    class ProcessInfo
-    {
+namespace funkin::utilities {
+	class ProcessInfo {
         public:
+            /**
+             * @see
+             * https://web.archive.org/web/20190923225212/http://nadeausoftware.com/articles/2012/07/c_c_tip_how_get_process_resident_set_size_physical_memory_use
+             */
             static std::size_t getMemoryUsage();
+            /**
+             * @see
+             * https://web.archive.org/web/20190923225212/http://nadeausoftware.com/articles/2012/07/c_c_tip_how_get_process_resident_set_size_physical_memory_use
+             */
             static std::size_t getMemoryPeak();
-    };
-    
-}
+	};
+
+} // namespace funkin::utilities
